8ac8c7c6e1c0c55c752185fe7b447c731eb5a3a2
A B M Ruman <abm.ruman@gmail.com>
2015-10-08 02:18:14 +0600

    Add HistoryActivity.java

---
+++ app/src/main/java/com/androidapps/ruman/calculator/HistoryActivity.java
@@ -0,0 +1,15 @@
+package com.androidapps.ruman.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ruman on 10/8/15.
+ */
+public class HistoryActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
