8282c50c80c409318cd532828995c59676575477
A B M Ruman <abm.ruman@gmail.com>
2015-10-08 18:23:18 +0600

    Add Back button to History Activity.

--- app/src/main/java/com/androidapps/ruman/calculator/HistoryActivity.java
+++ app/src/main/java/com/androidapps/ruman/calculator/HistoryActivity.java
@@ -2,6 +2,7 @@ package com.androidapps.ruman.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by ruman on 10/8/15.
@@ -11,5 +12,9 @@ public class HistoryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+    }
+
+    public void clickBack(View v) {
+        finish();
     }
 }
