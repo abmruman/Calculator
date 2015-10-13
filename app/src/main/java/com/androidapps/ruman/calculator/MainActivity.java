ed069fee2f51991e1a2615cf9bb8ae01cf114665
A B M Ruman <abm.ruman@gmail.com>
2015-10-13 13:28:40 +0600

    Implement GUI and add Click event to all buttons

--- app/src/main/java/com/androidapps/ruman/calculator/MainActivity.java
+++ app/src/main/java/com/androidapps/ruman/calculator/MainActivity.java
@@ -3,7 +3,11 @@ package com.androidapps.ruman.calculator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.style.ClickableSpan;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +17,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void clickNext(View v) {
+    public void History(View v) {
         startActivity(new Intent("com.androidapps.ruman.calculator.HISTORY_ACTIVITY"));
+    }
+
+
+    public void Click  (View v){
+        Button btn = (Button) v;
+        EditText editTextMain = (EditText) findViewById(R.id.editTextMain);
+        String strMain = editTextMain.getText().toString();
+        String txtBtn = btn.getText().toString();
+
     }
 }
