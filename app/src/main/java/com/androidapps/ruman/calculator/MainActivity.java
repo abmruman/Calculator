cdb8322a987725e17322cdc62b1562d32f6a0523
A B M Ruman <abm.ruman@gmail.com>
2015-10-15 11:58:22 +0600

    Change GUI a little bit and Change some variable names.

--- app/src/main/java/com/androidapps/ruman/calculator/MainActivity.java
+++ app/src/main/java/com/androidapps/ruman/calculator/MainActivity.java
@@ -3,18 +3,19 @@ package com.androidapps.ruman.calculator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.style.ClickableSpan;
-import android.util.Log;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textViewMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewMain = (TextView) findViewById(R.id.textViewMain);
     }
 
     public void History(View v) {
@@ -22,11 +23,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void Click  (View v){
-        Button btn = (Button) v;
-        EditText editTextMain = (EditText) findViewById(R.id.editTextMain);
-        String strMain = editTextMain.getText().toString();
-        String txtBtn = btn.getText().toString();
+    public void Click  (View view){
+        Button btn = (Button) view;
 
+        String strMain = textViewMain.getText().toString();
+        String txtBtn = btn.getText().toString();
+        switch (txtBtn){
+            case "=":
+                textViewMain.setText("0");
+                break;
+            case "DEL":
+                if(strMain.length()>0)
+                    textViewMain.setText(strMain.toCharArray(), 0, strMain.length()-1);
+                else
+                    textViewMain.setText("0");
+                break;
+            case "\u03C0":
+                txtBtn = getString(R.string.btn_pi);
+            default:
+                if(strMain.equals("0"))
+                    textViewMain.setText(txtBtn);
+                else
+                    textViewMain.append(txtBtn);
+                break;
+        }
     }
 }
